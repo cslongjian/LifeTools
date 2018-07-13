09a3fcd4edcc2cba1a372785c05f17d1e2baa253
cslongjian <chenlongjian@yaochufa.com>
2018-07-13 18:59:52 +0800

    文件

---
+++ LifeTools/app/src/main/java/com/cslong/app/lifetools/rxjavademo/CreateObservablesActivity.java
@@ -0,0 +1,28 @@
+package com.cslong.app.lifetools.rxjavademo;
+
+import android.os.Bundle;
+
+import com.cslong.app.lifetools.BaseActivity;
+
+import rx.Observable;
+
+
+/**
+ * 创建Observables方式
+ */
+public class CreateObservablesActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+    }
+
+
+    private Observable<Integer> createObserver() {
+
+    }
+
+
+}
