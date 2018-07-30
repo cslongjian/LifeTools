ee8556d7b3e659fa617473f182b5811bd1b0dfd5
cslongjian <chenlongjian@yaochufa.com>
2018-07-30 15:23:58 +0800

    操作线程API

--- LifeTools/app/src/main/java/com/cslong/app/lifetools/rxjavademo/CreateObservablesActivity.java
+++ LifeTools/app/src/main/java/com/cslong/app/lifetools/rxjavademo/CreateObservablesActivity.java
@@ -8,6 +8,9 @@ import com.cslong.app.lifetools.BaseActivity;
 import rx.Observable;
 import rx.Observer;
 import rx.Subscriber;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.functions.Action1;
+import rx.schedulers.Schedulers;
 
 
 /**
@@ -179,6 +182,56 @@ public class CreateObservablesActivity extends BaseActivity {
     {
         createObservable().subscribe(createObserver());
     }
+
+//      上面只介绍了。观察者 被观察者 订阅关系
+//      线程控制 Scheduler
+    /**
+     * 在不指定线程的情况下， RxJava 遵循的是线程不变的原则，
+     * 即：在哪个线程调用 subscribe()，就在哪个线程生产事件；
+     * 在哪个线程生产事件，就在哪个线程消费事件。
+     * 如果需要切换线程，就需要用到 Scheduler （调度器）。
+     *
+     * 1) Scheduler 的 API (一)
+     在RxJava 中，Scheduler ——调度器，相当于线程控制器，RxJava 通过它来指定每一段代码应该运行在什么样的线程。
+     RxJava 已经内置了几个 Scheduler ，它们已经适合大多数的使用场景：
+
+        1 Schedulers.immediate(): 直接在当前线程运行，相当于不指定线程。这是默认的 Scheduler。
+        2 Schedulers.newThread(): 总是启用新线程，并在新线程执行操作。
+        3 Schedulers.io(): I/O 操作（读写文件、读写数据库、网络信息交互等）所使用的 Scheduler。
+            行为模式和 newThread() 差不多，区别在于 io() 的内部实现是是用一个无数量上限的线程池，可以重用空闲的线程，
+            因此多数情况下 io() 比 newThread() 更有效率。不要把计算工作放在 io() 中，可以避免创建不必要的线程。
+        4 Schedulers.computation(): 计算所使用的 Scheduler。这个计算指的是 CPU 密集型计算，即不会被 I/O 等操作限制性能的操作，例如图形的计算。
+            这个 Scheduler 使用的固定的线程池，大小为 CPU 核数。不要把 I/O 操作放在 computation() 中，否则 I/O 操作的等待时间会浪费 CPU。
+        5 另外， Android 还有一个专用的 AndroidSchedulers.mainThread()，它指定的操作将在 Android 主线程运行。
+
+     *
+     * 有了这几个 Scheduler ，就可以使用 subscribeOn() 和 observeOn() 两个方法来对线程进行控制了。
+     * * subscribeOn(): 指定 subscribe() 所发生的线程，即 Observable.OnSubscribe 被激活时所处的线程。或者叫做事件产生的线程。
+     * * observeOn(): 指定 Subscriber 所运行在的线程。或者叫做事件消费的线程。
+     */
+
+//    来看个例子
+    private void SchedulerDemo() {
+        Observable.just(1, 2, 3, 4)
+                .subscribeOn(Schedulers.io()) // 指定 subscribe() 发生在 IO 线程
+                .observeOn(AndroidSchedulers.mainThread()) // 指定 Subscriber 的回调发生在主线程
+                .subscribe(new Action1<Integer>() {
+                    @Override
+                    public void call(Integer number) {
+                        Log.d("TTT", "number:" + number);
+                    }
+                });
+
+//      上面这段代码中，由于 subscribeOn(Schedulers.io()) 的指定，被创建的事件的内容 1、2、3、4 将会在 IO 线程发出；
+//      而由于 observeOn(AndroidScheculers.mainThread()) 的指定，因此 subscriber 数字的打印将发生在主线程 。
+//      事实上，这种在 subscribe() 之前写上两句 subscribeOn(Scheduler.io())
+//      和 observeOn(AndroidSchedulers.mainThread()) 的使用方式非常常见，
+//      它适用于多数的 『后台线程取数据，主线程显示』的程序策略。
+    }
+
+//    Scheduler 的原理 (一) 跟着变换一起讲解
+
+
 
 
 
