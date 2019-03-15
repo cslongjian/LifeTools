384ea69823335cd5d41e90a77fb80ebcee3112e9
cslongjian <chenlongjian@yaochufa.com>
2019-03-15 15:05:02 +0800

    持续添加完善

--- LifeTools/app/src/main/java/com/cslong/app/lifetools/designpatterndemo/create_pattern/factory/simplefactory/Chevrolet.java
+++ LifeTools/app/src/main/java/com/cslong/app/lifetools/designpatterndemo/create_pattern/factory/simplefactory/Chevrolet.java
@@ -4,6 +4,6 @@ public class Chevrolet extends ICar {
 
     @Override
     public void desc() {
-        System.out.println("通用旗下一哥，也是我的爱车品牌，雪佛兰");
+        System.out.println("通用旗下一哥，雪佛兰");
     }
 }
---
+++ LifeTools/app/src/main/java/com/cslong/app/lifetools/designpatterndemo/structure_pattern/decorator/DecoratorDemo.java
@@ -0,0 +1,8 @@
+package com.cslong.app.lifetools.designpatterndemo.structure_pattern.decorator;
+
+public class DecoratorDemo {
+
+    public static void main(String arg[]) {
+
+    }
+}
